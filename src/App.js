1f3729f65da8b3505a395ef1ef9c870144b00f48
SamarAhar <Tusharahar1906@gmail.com>
2024-01-21 07:49:21 +0530

    new

--- src/App.js
+++ src/App.js
@@ -9,14 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
       </header>
     </div>
   );
